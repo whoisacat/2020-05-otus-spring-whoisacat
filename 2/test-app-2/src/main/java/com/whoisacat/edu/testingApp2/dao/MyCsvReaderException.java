7d875c791882b4d9fb0021427b1fb9cd2e6392ce
whoisacat <whoisacat@gmail.com>
2020-06-06 20:56:50 +0500

    Revert "перевел проект на java-based и annotation-based конфигурацию"
    This reverts commit 74149c89

--- 2/test-app-2/src/main/java/com/whoisacat/edu/testingApp2/dao/MyCsvReaderException.java
+++
@@ -1,8 +0,0 @@
-package com.whoisacat.edu.testingApp2.dao;
-
-public class MyCsvReaderException extends Exception{
-
-    public MyCsvReaderException(){
-        super("somethingWrongWithCvReader");
-    }
-}
--- 2/test-app-2/src/main/java/com/whoisacat/edu/testingApp2/service/QuestionWriterService.java
+++
@@ -1,6 +0,0 @@
-package com.whoisacat.edu.testingApp2.service;
-
-public interface QuestionWriterService{
-
-    void writeQuestionsList();
-}
